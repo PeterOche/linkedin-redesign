124443f9265132f5728b3fe6ba85313d6f3b6522
PeterOche <petergoddey08@gmail.com>
2024-10-12 20:50:55 +0100

    yo

--- src/pages/Network.jsx
+++ src/pages/Network.jsx
@@ -7,15 +7,8 @@ const Network = () => {
     <div className="container lg:grid grid-cols-4 lg:gap-[40px] mx-auto px-8 md:px-24 mt-[40px]">
       <div className="">
         <LeftCard />
-        {/* <SortBy handleSort={handleSort} />
-        <PostsList posts={state.posts} users={state.users} /> */}
       </div>
       <div className="col-span-3 space-y-4">
-        {/* <SideProfileCard user={user} />
-        <NewArticleCard />
-        <GroupsCard groups={groups} />
-        <HashtagsCard />
-        <TrendingArticles articles={groups} /> */}
         <NetworkTabs />
       </div>
     </div>
--- src/pages/Notices.jsx
+++ src/pages/Notices.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import NetworkTabs from "../components/network/NetworkTabs";
+import StatCard from "../components/shared/StatCard";
 
 const Notices = () => {
-  return <div></div>;
+  return (
+    <div className="container lg:grid grid-cols-4 lg:gap-[40px] mx-auto px-8 md:px-24 mt-[40px]">
+      <div className="">
+        <StatCard />
+      </div>
+      <div className="col-span-3 space-y-4">
+        <NetworkTabs />
+      </div>
+    </div>
+  );
 };
 
 export default Notices;
